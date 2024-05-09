5bbc9402ea0bf43eb7bd16abe043c10f398848ac
PatrickYoung03 <patrickyoung18@googlemail.com>
2024-05-09 21:07:43 +0100

    Added list from radius, getById, deleteById, updateById

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,7 +1,7 @@
 const Job = require('../models/jobs')
+const geoCoder = require('../utils/geocoder')
 
 const getJobs = async (req, res, next) => {
-
     const jobs = await Job.find();
 
     res.status(200).send({
@@ -22,6 +22,93 @@ const createJob = async (req, res, next) => {
 
 };
 
+const getJobsInRadius = async (req, res, next) => {
+    const { zipcode, distance } = req.params
+
+    const loc = await geoCoder.geocode(zipcode)
+    const latitude = loc[0].latitude
+    const longitude = loc[0].longitude
+
+    const radius = distance / 3663
+
+    const jobs = await Job.find({
+        location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } },
+    })
+
+    res.status(200).send({
+        success: true,
+        results: jobs.length,
+        data: jobs
+    });
+}
+
+const updateJob = async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {
+        res.status(404).json({
+            success: false,
+            message: 'No job found with that ID'
+        });
+    };
+
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Job updated',
+        data: job
+    });
+};
+
+const deleteJob = async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {
+        res.status(404).json({
+            success: false,
+            message: 'No job found with that ID'
+        });
+    };
+
+    await Job.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+        success: true,
+        message: 'Job deleted'
+    });
+};
+
+const getJob = async (req, res, next) => {
+    let job = await Job.find({
+        $and: [
+            { _id: req.params.id },
+            { slug: req.params.slug }
+        ]
+    });
+
+    if (!job || job.length === 0) {
+        res.status(404).json({
+            success: false,
+            message: 'No job found with that ID'
+        });
+    };
+
+    res.status(200).json({
+        success: true,
+        message: 'Job found',
+        data: job
+    });
+};
+
 module.exports = {
-    getJobs, createJob
+    getJobs,
+    createJob,
+    getJobsInRadius,
+    updateJob,
+    deleteJob,
+    getJob
 }
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-const { getJobs, createJob } = require('../controllers/jobsController.js')
+const { getJobs, createJob, getJobsInRadius, updateJob, deleteJob, getJob } = require('../controllers/jobsController.js')
 
 
 const router = Router();
 
 router.route('/jobs').get(getJobs);
+
 router.route('/job/new').post(createJob);
+
+router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
+
+router.route('/job/:id').put(updateJob).delete(deleteJob)
+
+router.route('/job/:id/:slug').get(getJob);
 
 module.exports = router;
